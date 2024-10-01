48425c5cc50d11767c4da7c24c0ae351552b25b3
akelwalk <104277293+akelwalk@users.noreply.github.com>
2024-10-01 13:32:46 -0700

    started a next js app and added bootstrap

---
+++ nextjs_app/test_app/app/page.tsx
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1> Welcome to Almanac!</h1>
+      <div className="container">
+        <div className="row">
+          <div className = "col-sm">Column1</div>
+          <div className = "col-sm">Column2</div>
+          <div className = "col-sm">Column3</div>
+        </div>
+      </div>
+    </div>
+  );
+}
