826701d1aa0c724d09e9972367674d985499a412
neotqt <154855696+neotqt@users.noreply.github.com>
2025-02-18 19:11:35 -0700

    Using dynamically assigned ports in fetch calls.

--- nextjs_app/app/api/viability/route.tsx
+++ nextjs_app/app/api/viability/route.tsx
@@ -1,10 +1,27 @@
 import { NextResponse } from "next/server";
+import fs from 'fs';
+import path from 'path';
+
+function getAlmanacPortFromFile(): string | null {
+    try {
+      const filePath = path.join(process.cwd(), 'flask_backend', 'flask_port.json');
+      const fileData = fs.readFileSync(filePath, 'utf-8');
+      const jsonData = JSON.parse(fileData);
+      return jsonData.port;
+    } catch (error) {
+      console.error("Error reading flask_port.json:", error);
+      return null;
+    }
+  }
+  
 
 export async function GET() {
     // Forward the GET request to the Flask server 
     // console.log("In viability agent route");
-    const flaskResponse = await fetch("http://localhost:5000/viability", {method: "GET"});
-    // console.log("Done fetching response");
+    const port = getAlmanacPortFromFile();
+    console.log("Using Flask port:", port);
+    const flaskResponse = await fetch(`http://localhost:${port}/viability`, { method: "GET" });
+
     if (!flaskResponse.ok) {
         return NextResponse.json({ message: 'Error from Flask server' }, { status: flaskResponse.status });
     }
