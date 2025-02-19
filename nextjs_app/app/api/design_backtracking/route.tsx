68cb5c6918905fad978d3db4e7305aac9eb9e1a6
neotqt <154855696+neotqt@users.noreply.github.com>
2025-02-18 19:10:44 -0700

    Using dynamically assigned ports in fetch calls.

--- nextjs_app/app/api/design_backtracking/route.tsx
+++ nextjs_app/app/api/design_backtracking/route.tsx
@@ -1,9 +1,24 @@
 import { NextResponse } from "next/server";
+import fs from 'fs';
+import path from 'path';
+
+function getAlmanacPortFromFile(): string | null {
+    try {
+      const filePath = path.join(process.cwd(), 'flask_backend', 'flask_port.json');
+      const fileData = fs.readFileSync(filePath, 'utf-8');
+      const jsonData = JSON.parse(fileData);
+      return jsonData.port;
+    } catch (error) {
+      console.error("Error reading flask_port.json:", error);
+      return null;
+    }
+  }
 
 export async function GET() {
     // Forward the GET request to the Flask server 
-    console.log("in backtracking route")
-    const flaskResponse = await fetch("http://localhost:5000/design_backtracking", {method: "GET"});
+    const port = getAlmanacPortFromFile();
+    console.log("Using Flask port:", port);
+    const flaskResponse = await fetch(`http://localhost:${port}/design_backtracking`, {method: "GET"});
     if (!flaskResponse.ok) {
         return NextResponse.json({ message: 'Error from Flask server' }, { status: flaskResponse.status });
     }
