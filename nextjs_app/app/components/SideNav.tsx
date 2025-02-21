a8381e84ba8c42cb46c0990d7dce4cfa3a8e352f
akelwalk <104277293+akelwalk@users.noreply.github.com>
2025-02-21 14:54:36 -0700

    swe agent front end start

---
+++ nextjs_app/app/components/SideNav.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import "../globals.css";
+
+const SideNav: React.FC = () => {
+  return (
+    <nav className="sidenav">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#features">Features</a></li>
+        <li><a href="#tutorials">Tutorials</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNav;
