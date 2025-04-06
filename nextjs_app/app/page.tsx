75ce222f65364e32540ff820ddc2a0d9e713364f
Ashley Hummel <ashley.l.hummel@gmail.com>
2025-04-06 12:50:56 -0700

    Update and improve Home page

--- nextjs_app/app/page.tsx
+++ nextjs_app/app/page.tsx
@@ -4,16 +4,18 @@ import './styles/agents.css';
 
 export default function Home() {
   return (
-    <main style={{ marginTop: "2rem" }}>
-      <h1 className="main-title">AGENTVILLE</h1>
-      <p className="main-subtitle">Automate the creation of MVPs</p>
+    <main style={{ marginTop: "1rem" }}>
+      <div className="home-container">
+        <h1 className="main-title">ALMANAC</h1>
+        <p className="main-subtitle">Automate the creation of MVPs</p>
 
-      {/* Get Started Button */}
-      <Link href="/design_agent_input">
-        <button className="button mt-8" style={{ fontSize: "1rem" }}>
-          Get Started
-        </button>
-      </Link>
+        {/* Get Started Button */}
+        <Link href="/design_agent_input">
+          <button className="button mt-8" style={{ fontSize: "1rem" }}>
+            Get Started
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
